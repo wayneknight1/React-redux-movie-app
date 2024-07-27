1dc9d3937c8a8867615694b970f2ca835f4eaf05
wayneknight1 <pranavvenkata48@gmail.com>
2024-07-27 19:58:13 +0530

    Tested out the omdb api call with the provided api key

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,25 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import MovieListing from '../MovieListing/MovieListing'
+import movieApi from '../../common/apis/movieApi'
+import { APIKey } from '../../common/apis/MovieApiKey'
 function Home() {
+    useEffect(() => {
+        console.log('Inside the useEffect in home component the api key is ',APIKey)
+        const movieText = 'Harry';
+        const fetchMovies = async () => {
+            const response = await movieApi.get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+            .catch(err => {
+                console.log('Err: ',err)
+            })
+            console.log('response from api is ',response)
+        }
+        fetchMovies()
+    },[]) 
   return (
-    <div>Home</div>
+    <div>
+        <div className='banner-img'></div>
+        <MovieListing/>
+    </div>
   )
 }
 
