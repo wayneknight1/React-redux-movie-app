e4ee799c62744286661a2a8939ca7c6318c1d1f8
wayneknight1 <pranavvenkata48@gmail.com>
2024-07-27 22:42:20 +0530

    Dealt with the MovieListing component

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,8 +1,30 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { getAllMovies } from '../../features/movies/movieSlice'
+import MovieCard from '../MovieCard/MovieCard'
+import './MovieListing.scss'
 function MovieListing() {
+    const movies = useSelector(getAllMovies)
+    useEffect(() => {
+        // console.log(`movies are ${JSON.stringify(movies)}`)
+    },[])
+    let renderMovies = '';
+    renderMovies = movies.Response === 'True'? (movies.Search.map((movie,index) => {
+        return <MovieCard key = {index} data = {movie}/>
+    })) :(
+        <div className='movies-error'>
+            <h3>{movies.Error}</h3>
+        </div>
+    );
   return (
-    <div>MovieListing</div>
+    <div className='movie-wrapper'>
+        <div className='movie-list'>
+            <h2>Movies</h2>
+            <div className='movie-container'>
+                {renderMovies}
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/features/store.js
+++ src/features/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from './movies/movieSlice'
 export const store = configureStore({
-    reducer: moviesReducer
+    reducer: {
+        movies: moviesReducer
+    }
 })
