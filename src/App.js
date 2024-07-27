650abc54045d6142916372b9b9de40c13990404c
wayneknight1 <pranavvenkata48@gmail.com>
2024-07-27 18:25:36 +0530

    Added routing logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import './App.scss'
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import MovieDetail from './components/MovieDetail/MovieDetail';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
-    <div>App</div>
+    <div className='app'>
+        <Router>
+            <Header></Header>
+            <Routes>
+            <Route path='/' Component={Home}/>
+            <Route path='/movie/:imdbId' Component={MovieDetail}/>
+            <Route Component={PageNotFound}/>
+            </Routes>
+            <Footer/>
+        </Router>
+    </div>
   )
 }
 
---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey = '459565f6'
