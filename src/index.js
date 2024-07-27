436c8d8dd25d16ea2b4936323403f9b322a3173c
wayneknight1 <pranavvenkata48@gmail.com>
2024-07-27 22:03:44 +0530

    configured redux store and created mviesSlice

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Provider } from 'react-redux'
+import { store } from './features/store'
 const el = document.querySelector('#root')
 const root = ReactDOM.createRoot(el)
-root.render(<App/>)
+root.render(<Provider store={store}><App/></Provider>)
