f0259cd99c005e20ef75e4f895c8433027328e0f
wayneknight1 <pranavvenkata48@gmail.com>
2024-07-28 18:14:44 +0530

    async thunk for api calls

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,12 @@
 import React, { useEffect } from 'react'
 import MovieListing from '../MovieListing/MovieListing'
-import movieApi from '../../common/apis/movieApi'
 import { APIKey } from '../../common/apis/MovieApiKey'
 import { useDispatch } from 'react-redux'
-import { addMovies } from '../../features/movies/movieSlice'
+import { fetchAsyncMovies } from '../../features/movies/movieSlice'
 function Home() {
     const dispatch = useDispatch()
     useEffect(() => {
-        console.log('Inside the useEffect in home component the api key is ',APIKey)
-        const movieText = 'Harry';
-        const fetchMovies = async () => {
-            const response = await movieApi.get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
-            .catch(err => {
-                console.log('Err: ',err)
-            })
-            console.log('response from api is ',response)
-            dispatch(addMovies(response.data))
-        }
-        fetchMovies()
+        dispatch(fetchAsyncMovies())
     },[]) 
   return (
     <div>
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,4 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import movieApi from '../../common/apis/movieApi'
+import { APIKey } from '../../common/apis/MovieApiKey'
+import { useDispatch } from 'react-redux'
+
+
+export const fetchAsyncMovies = createAsyncThunk('movies/fetchAsyncMovies', async () => {
+    const movieText = 'Harry';
+    const response = await movieApi.get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+            .catch(err => {
+                console.log('Err: ',err)
+            })
+            console.log('response from api is ',response)
+            return response.data
+})
 
 const initialState = {
     movies: {}
@@ -11,7 +25,19 @@ const movieSlice = createSlice({
         addMovies: (state,{payload}) => {
             state.movies = payload
         }
-    }
+    },
+    extraReducers: builder => { //significatly different. try debugging this if issues raise
+        builder
+        .addCase(fetchAsyncMovies.pending, state => {
+            console.log('pending')
+        })
+        .addCase(fetchAsyncMovies.fulfilled,(state,action)=>{
+            return {...state,movies: action.payload}
+        })
+        .addCase(fetchAsyncMovies.rejected, (state,action) => {
+            console.log('Rejected!')
+        })
+    }   
 })
 
 export const {addMovies} = movieSlice.actions //self note - action creator. 
