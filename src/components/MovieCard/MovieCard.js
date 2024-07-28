543ee715e32041fe61cc244bb9b5f81b1d9efa77
wayneknight1 <pranavvenkata48@gmail.com>
2024-07-28 15:43:50 +0530

    styled movie card component

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
-function MovieCard() {
+import './MovieCard.scss'
+function MovieCard({data}) {
   return (
-    <div>MovieCard</div>
+    <div className='card-item'>
+      <div className='card-inner'>
+        <div className='card-top'>
+          <img src={data.Poster} alt={data.title}/>
+        </div>
+        <div className='card-bottom'>
+            <div className='card-info'>
+                <h4>{data.Title}</h4>
+                <p>{data.Year}</p>
+            </div>  
+        </div>
+      </div>
+    </div>
   )
 }
 
