7ca1a16e6eec5eea4fe97f260d63b5b2724bda31
wayneknight1 <pranavvenkata48@gmail.com>
2024-07-29 19:05:49 +0530

    Added the movie detail component

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { fetchMovieOrShowDetail } from '../../features/movies/movieSlice'
 
 function MovieDetail() {
+  const {imdbID} = useParams()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchMovieOrShowDetail(imdbID))
+  },[dispatch,imdbID])
+
   return (
     <div>MovieDetail</div>
   )
